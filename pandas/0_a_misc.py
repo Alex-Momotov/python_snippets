6597bb436f9d1fe683e8e777fcf7e2ef3747bc55
Alex Momotov <alex.momotov@vonage.com>
2021-02-23 17:47:59 +0000

    Renamed pandas notes

--- pandas/0_a_misc.py
+++
@@ -1,5 +0,0 @@
-# Basic analytics
-
-# Series.value_counts()
-# Returns count of unique values in the Series. By default ignores None values, so pass dropna=False to avoid that.
-df.Athlete.value_counts(dropna=False)
